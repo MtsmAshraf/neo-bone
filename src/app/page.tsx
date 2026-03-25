b48dd5bb385beb18bc5877a123a8204d7e49ead7
moatasim <mo32000a@gmail.com>
2026-03-25 17:34:29 +0200

    added coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "../components/AboutSection";
 import CasesSection from "../components/CasesSection";
+import ComingSoon from "../components/ComingSoon";
 import ContactSection from "../components/ContactSection";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
@@ -9,8 +10,9 @@ import TestimonialsSection from "../components/TestimonialsSection";
 import "./globals.css";
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
+    <main className="min-h-screen bg-background relative h-screen overflow-hidden">
       <Navbar />
+      <ComingSoon />
       <HeroSection />
       <AboutSection />
       <ServicesSection />
---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,11 @@
+
+const ComingSoon = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }} className='z-50 bg-white h-screen w-full absolute top-0 left-0 flex flex-col items-center justify-center'>
+        <h1 className='text-4xl mb-1'>Coming Soon</h1>
+        <p>We're working on something great. Check back soon.</p>
+    </div>
+  )
+}
+
+export default ComingSoon
